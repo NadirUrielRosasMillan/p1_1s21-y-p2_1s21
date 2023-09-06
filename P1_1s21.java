cf28a2ff81ab993f6204681e7624869765c41c5b
Nadir Uriel Rosas Millan <nadirrosas2905@gmail.com>
2023-09-06 11:05:35 -0600

    2 practicas
    primeras 2

---
+++ P1_1s21.java
@@ -0,0 +1,7 @@
+class p1_1s21 {
+    public static void main(String[] args) {
+
+        System.out.println("Este es mi primer promgrama");
+
+    }
+}
---
+++ P2_1s21.java
@@ -0,0 +1,15 @@
+public class P2_1s21 {
+
+        public static void main(String[] args) {
+           
+           
+            //ln me permite mi salto de pagina//
+            System.out.println("Nombre: Nadir ");
+            System.out.println("Edad: 18 años");
+            System.out.println("Correo electronico: nadirrosas2905@gmail.com");
+            System.out.println("Aficionado: Box");
+            System.out.println("Color Favrito: Azul");
+    
+        }
+    }
+    
